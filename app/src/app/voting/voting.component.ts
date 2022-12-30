b307d6faca35b1c4bc7448aed7c0a3dfb98a6947
Lucie Hromádková <luci.hromadko@gmail.com>
2022-12-30 03:21:03 +0200

    table visuals

--- app/src/app/voting/voting.component.ts
+++ app/src/app/voting/voting.component.ts
@@ -21,6 +21,9 @@ export class VotingComponent implements OnInit {
   voted = false
   questionId: string
   showResults: boolean
+  data: TableRow[]
+  displayedColumns: string[]
+  displayedHouses: string[]
 
   ngOnInit() {
     this.db.object("landsraad/currentQuestion").valueChanges().pipe(
@@ -39,18 +42,27 @@ export class VotingComponent implements OnInit {
       })
     ).subscribe()
     let delegateId = this.auth.auth.currentUser.uid
+    console.log("delegate id", delegateId)
     this.votingRights = this.db.list("landsraad/votingRights", ref => ref.orderByChild("controlledBy").equalTo(delegateId)).snapshotChanges().pipe(
       map(
         snapshots => {
           return snapshots.map(snapshot => {
             let value = snapshot.payload.val()
+            console.log({ id: snapshot.key, name: value["name"], votes: value["votes"] })
             return { id: snapshot.key, name: value["name"], votes: value["votes"] }
           })
         })
     )
+    this.setData()
+    this.setDisplayedHouses()
+    console.log("houses", typeof(this.displayedHouses), this.displayedHouses)
+    this.setDisplayedColumns()
+    console.log("columns", typeof(this.displayedColumns), this.displayedColumns)
+    console.log("\nright", this.votingRights.pipe(take(1)))
     // TODO maybe change resultsShown to showResults also elsewhere as it sounds better
     this.db.object("landsraad/votingConfig/resultsShown").valueChanges().subscribe((value: boolean) => {
       this.showResults = value
+      console.log(this.showResults)
     })
   }
 
@@ -70,10 +82,36 @@ export class VotingComponent implements OnInit {
     }
   }
 
+  setData() {
+    this.data = [
+      {dekret: "This is really loooooooooooooooooooo question", name: 'H', weight: 19, symbol: 'H', total: 6},
+      {dekret: "b", name: 'H', weight: 46, symbol: 'He', total: 5},
+      {dekret: "c", name: 'L', weight: 6, symbol: 'Li', total: 4},
+      {dekret: "Celkem hlasů na rod", name: "prvky", weight: 24, symbol: "HHeLi", total: 15}
+    ];
+  }
+
+  setDisplayedHouses() {
+    this.displayedHouses = ["name", "weight", "symbol"]
+  }
+
+  setDisplayedColumns() {
+    let columns = this.displayedHouses.slice()
+    columns.unshift("dekret")
+    columns.push("total")
+    this.displayedColumns = columns
+  }
+
 }
 
 interface VotingRight {
   id: string,
   name: string,
   votes: number
+}
+
+export interface TableRow {
+  dekret: string;
+  total: number;
+  [key: string]: string | number;
 }
