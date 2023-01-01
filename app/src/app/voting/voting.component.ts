d60c0e24ecfedba7c962d8ae4dc197c779e15d39
Lucie Hromádková <luci.hromadko@gmail.com>
2023-01-02 01:29:13 +0200

    WIP: add real data to table

--- app/src/app/voting/voting.component.ts
+++ app/src/app/voting/voting.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAction, AngularFireDatabase, DatabaseSnapshot } from '@angular/fire/database';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
 import { ValueName } from '../../../../common/config';
 
@@ -23,7 +23,7 @@ export class VotingComponent implements OnInit {
   showResults: boolean
   data: TableRow[]
   displayedColumns: string[]
-  displayedHouses: string[]
+  displayedHouses: string[]  // all the delegations with voting right to the question
 
   ngOnInit() {
     this.db.object("landsraad/currentQuestion").valueChanges().pipe(
@@ -42,17 +42,16 @@ export class VotingComponent implements OnInit {
       })
     ).subscribe()
     let delegateId = this.auth.auth.currentUser.uid
-    console.log("delegate id", delegateId)
     this.votingRights = this.db.list("landsraad/votingRights", ref => ref.orderByChild("controlledBy").equalTo(delegateId)).snapshotChanges().pipe(
       map(
         snapshots => {
           return snapshots.map(snapshot => {
             let value = snapshot.payload.val()
-            console.log({ id: snapshot.key, name: value["name"], votes: value["votes"] })
             return { id: snapshot.key, name: value["name"], votes: value["votes"] }
           })
         })
     )
+    // TODO this is temp, displayedHouses, columns and data will be set in setData()
     this.setData()
     this.setDisplayedHouses()
     console.log("houses", typeof(this.displayedHouses), this.displayedHouses)
@@ -62,7 +61,10 @@ export class VotingComponent implements OnInit {
     // TODO maybe change resultsShown to showResults also elsewhere as it sounds better
     this.db.object("landsraad/votingConfig/resultsShown").valueChanges().subscribe((value: boolean) => {
       this.showResults = value
-      console.log(this.showResults)
+      if (this.showResults) {
+        console.log("set data")
+        this.composeData()
+      }
     })
   }
 
@@ -82,17 +84,55 @@ export class VotingComponent implements OnInit {
     }
   }
 
+  composeData() {
+    // TODO this is deprecated, check how to refactor "to pipe to a map"
+    combineLatest(
+      this.db.list("landsraad/votes/" + this.questionId).snapshotChanges(),
+      this.db.list("landsraad/answers/"+this.questionId).snapshotChanges(),
+      this.db.list("landsraad/votingRights").snapshotChanges(),
+      (votes, answers, votingRights) => {
+        return { votes: votes, answers: answers, votingRights: votingRights }
+      }
+    ).pipe(
+      take(1),
+      tap(
+        combined => {
+          let data = combined.answers.map(answerSnap => {
+            let answerName = answerSnap.payload.val()["name"]
+            let answerId = answerSnap.key
+            let row = [answerName]
+            combined.votingRights.forEach(votingRightSnap => {
+              let votingRightId = votingRightSnap.key
+              row.push(findVote(combined.votes, votingRightId, answerId))
+              console.log("findVote", findVote(combined.votes, votingRightId, answerId))
+            })
+            return row
+          })
+          let headers = ["Odpověď"]
+          combined.votingRights.forEach(votingRightSnap => {
+            headers.push(votingRightSnap.payload.val()["name"])
+          })
+          let options = {
+            headers: headers
+          };
+          console.log("combined")
+          console.log(combined)
+        }
+      )
+    ).subscribe()
+  }
+
   setData() {
     this.data = [
-      {dekret: "This is really loooooooooooooooooooo question", name: 'H', weight: 19, symbol: 'H', total: 6},
-      {dekret: "b", name: 'H', weight: 46, symbol: 'He', total: 5},
-      {dekret: "c", name: 'L', weight: 6, symbol: 'Li', total: 4},
-      {dekret: "Celkem hlasů na rod", name: "prvky", weight: 24, symbol: "HHeLi", total: 15}
+      {dekret: "This is really loooooooooooooooooooo question", Yuzovka: 6, Fenring: 19, Atreides: 7, total: 32},
+      {dekret: "b", Yuzovka: 2, Fenring: 46, Atreides: 5, total: 53},
+      {dekret: "c", Yuzovka: 5, Fenring: 6, Atreides: 1, total: 12},
+      {dekret: "Celkem hlasů na rod", Yuzovka: 13, Fenring: 71, Atreides: 13, total: 97}
     ];
   }
 
   setDisplayedHouses() {
-    this.displayedHouses = ["name", "weight", "symbol"]
+    this.displayedHouses = ["Yuzovka", "Fenring", "Atreides"]
   }
 
   setDisplayedColumns() {
@@ -102,6 +142,19 @@ export class VotingComponent implements OnInit {
     this.displayedColumns = columns
   }
 
+}
+
+// TODO taken from swiss/src/app/question-form/question-form.components.ts -> should be refactored to common
+function findVote(snapshots: AngularFireAction<DatabaseSnapshot<any>>[], votingRightId: string, answerId: string) {
+  let snapshot = snapshots.find((row) => row.key == votingRightId);
+  if (snapshot == null) {
+    return ""
+  }
+  let votes = snapshot.payload.val()[answerId]
+  if (votes == null) {
+    return ""
+  }
+  return votes
 }
 
 interface VotingRight {
